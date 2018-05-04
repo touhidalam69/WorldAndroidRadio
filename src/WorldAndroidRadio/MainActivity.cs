4a970ac459539903c399977cd3fb995576d4147c
Md. Touhid Alam <14317784+touhidalam69@users.noreply.github.com>
2018-05-04 19:20:18 +0600

    Create New Project on Visual Studio
    Created a new android visual studio Project

---
+++ src/WorldAndroidRadio/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace WorldAndroidRadio
+{
+    [Activity(Label = "WorldAndroidRadio", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+        }
+    }
+}
+
