4e8b6f23f20593e04f31b9d3a68541853ba21e3b
Md. Touhid Alam <14317784+touhidalam69@users.noreply.github.com>
2018-05-05 22:44:33 +0600

    Added main layout and configure application
    Added main layout and configure project Properties

--- src/WorldAndroidRadio/MainActivity.cs
+++ src/WorldAndroidRadio/MainActivity.cs
@@ -1,15 +1,18 @@
-﻿using Android.App;
+﻿using Android;
+using Android.App;
 using Android.OS;
 
 namespace WorldAndroidRadio
 {
-    [Activity(Label = "WorldAndroidRadio", MainLauncher = true)]
+    [Activity(Label = "World Radio", MainLauncher = true)]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
         }
     }
 }
