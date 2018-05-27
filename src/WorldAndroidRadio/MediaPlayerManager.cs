f4d4cf658c5ae0aa54fbe89a975acf2ca6de65ec
Md. Touhid Alam <14317784+touhidalam69@users.noreply.github.com>
2018-05-28 01:20:35 +0600

    Update Styles Added Updated APK
    Added Updated apk file, button style changed, Update Minifest and comment added on code of MediaPlayerManager Class

--- src/WorldAndroidRadio/MediaPlayerManager.cs
+++ src/WorldAndroidRadio/MediaPlayerManager.cs
@@ -8,20 +8,27 @@ namespace WorldAndroidRadio
 {
     public class MediaPlayerManager
     {
+        // Declaring Mediaplayer
         public static MediaPlayer player;
+
+        // Declaring ProgressDialog
         public static ProgressDialog mDialog;
         public static async void Play(string Url, Context context)
         {
             if (player == null)
             {
+                // Creating new instance of Mediaplayer
                 player = new MediaPlayer();
                 player.SetAudioStreamType(Stream.Music);
             }
             else {
                 player.Reset();
+                // IF Stoped Stop button will be hide
+                MainActivity.btnPlay.Visibility = ViewStates.Invisible;
             }
             try
             {
+                // ProgressDialog will be apeared before playing
                 await player.SetDataSourceAsync(Url);
                 player.PrepareAsync();
                 mDialog = new ProgressDialog(context);
@@ -33,6 +40,7 @@ namespace WorldAndroidRadio
             {
                 Toast.MakeText(context, "Radio Station not Responding", ToastLength.Long).Show();
             }
+
             player.Prepared += Player_Prepared;
             player.Error += (sender, args) =>
             {
@@ -46,23 +54,32 @@ namespace WorldAndroidRadio
         {
             player.Start();
             mDialog.Dismiss();
+            // IF Stoped Stop button will be showen
+            MainActivity.btnPlay.Visibility = ViewStates.Visible;
         }
 
+        // This function will check the player mode (Playing/Stopped)
         public static bool CheckPlayer()
         {
             if (player != null)
             {
                 if (player.IsPlaying)
                 {
+                    // IF Stoped Stop button will be showen
+                    MainActivity.btnPlay.Visibility = ViewStates.Visible;
                     return true;
                 }
                 else
                 {
+                    // IF Stoped Stop button will be hide
+                    MainActivity.btnPlay.Visibility = ViewStates.Invisible;
                     return false;
                 }
             }
             else
             {
+                // IF Stoped Stop button will be hide
+                MainActivity.btnPlay.Visibility = ViewStates.Invisible;
                 return false;
             }
         }
@@ -72,6 +89,8 @@ namespace WorldAndroidRadio
             {
                 player.Stop();
                 player.Reset();
+                // IF Stoped Stop button will be hide
+                MainActivity.btnPlay.Visibility = ViewStates.Invisible;
             }
 
             return true;
